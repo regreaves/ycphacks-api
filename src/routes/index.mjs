9380121aa28fe1316a55891f0a90948ff078a63f
Ralph Greaves <rgreaves@ycp.edu>
2023-02-06 20:19:42 -0500

    Create index.mjs

---
+++ src/routes/index.mjs
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import { hardware } from './hardware.mjs';
+
+const router = express.Router();
+
+router.use(express.json());
+
+const routes = [
+  hardware
+];
+
+router.use('/api', routes);
+
+export { router }
