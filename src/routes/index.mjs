5223798e1144276e43469a3e8c500292cd0b4ddc
Ralph Greaves <rgreaves@ycp.edu>
2023-03-02 13:25:10 -0500

    Update index.mjs

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -2,15 +2,13 @@ import express from 'express';
 
 import { auth, requiredScopes } from 'express-oauth2-jwt-bearer';
 
-import { get_hardware_items } from '../lib/database.mjs';
-
 const router = express.Router();
 
 router.use(auth());
 
 router.route('/api/v1/hardware')
   .get(requiredScopes('read:hardware'), async (req, res, next) => {
-    const data = await get_hardware_items();
+    const data = { key: 'value' };
 
     console.log(data);
 
