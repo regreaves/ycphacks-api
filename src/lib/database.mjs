2c79a274bb96578084add93aeec22d1cab7a1660
Ralph Greaves <rgreaves@ycp.edu>
2023-03-02 13:11:22 -0500

    Update database.mjs

--- src/lib/database.mjs
+++ src/lib/database.mjs
@@ -1,17 +1,17 @@
 import mysqlx from '@mysql/xdevapi';
 
-export const config = {
-  user: process.env.MYSQLX_USER,
-  password: process.env.MYSQLX_PASSWORD,
-  host: process.env.MYSQLX_HOST,
-  port: Number.parseInt(process.env.MYSQLX_PORT),
-  schema: process.env.MYSQLX_SCHEMA,
-  tls: {
-    enabled: true
-  }
-};
-
-export async function list_hardware_items(config) {
+export async function get_hardware_items() {
+  config = {
+    user: process.env.MYSQLX_USER,
+    password: process.env.MYSQLX_PASSWORD,
+    host: process.env.MYSQLX_HOST,
+    port: Number.parseInt(process.env.MYSQLX_PORT),
+    schema: process.env.MYSQLX_SCHEMA,
+    tls: {
+      enabled: true
+    }
+  };
+  
   try {
     const session = await mysqlx.getSession(config);
     
