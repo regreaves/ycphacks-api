38ab08bfa1a8485175b3ccbc7423f70ec68d2539
Ralph Greaves <rgreaves@ycp.edu>
2023-03-02 13:10:03 -0500

    Create database.mjs

---
+++ src/lib/database.mjs
@@ -0,0 +1,24 @@
+import mysqlx from '@mysql/xdevapi';
+
+export const config = {
+  user: process.env.MYSQLX_USER,
+  password: process.env.MYSQLX_PASSWORD,
+  host: process.env.MYSQLX_HOST,
+  port: Number.parseInt(process.env.MYSQLX_PORT),
+  schema: process.env.MYSQLX_SCHEMA,
+  tls: {
+    enabled: true
+  }
+};
+
+export async function list_hardware_items(config) {
+  try {
+    const session = await mysqlx.getSession(config);
+    
+    const result = await session.sql('CALL get_hardware_items();').execute();
+
+    return await result.fetchAll();
+  } catch (error) {
+    console.error(error);
+  }
+}
