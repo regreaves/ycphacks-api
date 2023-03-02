3292d5c33ad17eb59c1e9bf1ac33f5ee56a336d2
Ralph Greaves <rgreaves@ycp.edu>
2023-03-02 17:31:29 -0500

    Update index.mjs

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -4,13 +4,18 @@ import { auth, requiredScopes } from 'express-oauth2-jwt-bearer';
 
 const router = express.Router();
 
+router.route('/api/v1/sponsors')
+  .get((req, res) => {
+    const data = { key: 'value' };
+
+    res.status(200).json(data);
+  });
+
 router.use(auth());
 
 router.route('/api/v1/hardware')
-  .get(requiredScopes('read:hardware'), async (req, res, next) => {
+  .get(requiredScopes('read:hardware'), async (req, res) => {
     const data = { key: 'value' };
-
-    console.log(data);
 
     res.status(200).json(data);
   });
