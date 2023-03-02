4051fe75c061b3392be99d48c57e3146b2e669ec
Ralph Greaves <rgreaves@ycp.edu>
2023-03-02 12:41:17 -0500

    Update index.mjs

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -4,7 +4,10 @@ import { auth, requiredScopes } from 'express-oauth2-jwt-bearer';
 
 const router = express.Router();
 
-router.use(auth());
+router.use(auth({
+  issuerBaseURL: 'https://dev-00248e57.us.auth0.com/',
+  audience: 'https://api'
+}));
 
 router.route('/v1/hardware')
   .get(requiredScopes('read:hardware'), async (req, res, next) => {
