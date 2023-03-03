4c453c3855ab128b04f8adbf6daa263163c6a8ec
Ralph Greaves <rgreaves@ycp.edu>
2023-03-02 21:28:55 -0500

    Update hardware.mjs

--- src/routes/hardware.mjs
+++ src/routes/hardware.mjs
@@ -13,7 +13,7 @@ router.route('/sponsors')
 
 router.route('/hardware')
   .get((req, res) => {
-    const data = { key: 'value' };
+    const data = [["10908791783496351755","name","link","category","available","2023-02-27T04:24:50.000Z","2023-02-27T04:24:50.000Z","location"],["10908791783496351756","name","link","category","available","2023-02-27T04:24:50.000Z","2023-02-27T04:24:50.000Z","location"],["10908791783496351757","name","link","category","available","2023-02-27T04:24:50.000Z","2023-02-27T04:24:50.000Z","location"],["10908791783496351758","name","link","category","available","2023-02-27T04:24:50.000Z","2023-02-27T04:24:50.000Z","location"]];
 
     res.status(200).json(data);
   });
