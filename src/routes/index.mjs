e16e5d4f6b62a9ebcbb6ee3ab00f72065470dfd5
Ralph Greaves <rgreaves@ycp.edu>
2023-03-02 21:00:52 -0500

    Update index.mjs

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -4,28 +4,10 @@ import { hardware } from './hardware.mjs';
 
 const router = express.Router();
 
-<<<<<<< Updated upstream
-router.route('/api/v1/sponsors')
-  .get((req, res) => {
-    const data = { key: 'value' };
-
-    res.status(200).json(data);
-  });
-
-router.use(auth());
-
-router.route('/api/v1/hardware')
-  .get(requiredScopes('read:hardware'), async (req, res) => {
-    const data = { key: 'value' };
-
-    res.status(200).json(data);
-  });
-=======
 const routes = [
   hardware
 ];
 
 router.use('/api/v1', routes);
->>>>>>> Stashed changes
 
 export { router };
